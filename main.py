3e425ae8c4f85327cfa0c468bd6b9c44589ee191
m <test@example.com>
2026-04-30 01:19:22 -0700

    wellll

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Body
+
 
 app = FastAPI()
 
@@ -6,3 +7,34 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+
+
+
+@app.post("/captchas/verify/cf-turnstile")
+async def explodeCFTurnstle(token:str):
+    # this is NOT the right approach to get query and body lol
+    # ig i need types and stuff, tmr
+    # rn its just curl -X POST  "localhost:8000/captchas/verify/cf-turnstile?token=mrrp"
+    print(token)
+    return {"message": token}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
