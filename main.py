5dd1e9012a2fee7558b0c33cf01266d1647aae77
m <test@example.com>
2026-05-01 00:01:54 -0700

    .... 30 minutes debugging cf keys and uhh not done

---
+++ main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, Query, Body
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+challenge_pages = Jinja2Templates(directory= "src/captchas/challenges")
+
+# @app.get("/captchas/challenge/cf-turnstile")
+@app.get("/captchas/cf-turnstile")
+async def serveTurnstile():
+    # a
+    #todo: serve htmls
+    # return True
+    return challenge_pages.TemplateResponse(
+        request={},
+        name="cf-turnstile.html"
+    )
+
+
+@app.post("/captchas/verify/cf-turnstile")
+async def explodeCFTurnstle(token:str):
+    # this is NOT the right approach to get query and body lol
+    # ig i need types and stuff, tmr
+    # rn its just curl -X POST  "localhost:8000/captchas/verify/cf-turnstile?token=mrrp"
+    print(token)
+
+    # fix body logic above
+
+    # cf: POST https://challenges.cloudflare.com/turnstile/v0/siteverify
+    return {"message": token}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
