b5cba25cfc947041c0f1119c56004dc683cead19
m <test@example.com>
2026-05-21 21:36:21 -0700

    it took me too long to make a request counter lol. feature yayyy. now it writes to my dumb sqlite db

---
+++ src/poorusers.py
@@ -0,0 +1,115 @@
+# code based on example from fastapi wesbsite
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    username: str = Field(index=True)
+    cloudflare_turnstiles_solved: int = Field(default=0)
+
+
+# class Hero(SQLModel, table=True):
+#     id: int | None = Field(default=None, primary_key=True)
+#     name: str = Field(index=True)
+#     age: int | None = Field(default=None, index=True)
+#     secret_name: str
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+# https://ipv4.games/claim?name=whatever
+@app.get("/claim")
+def claim(name: str, session: SessionDep):
+    # if usr hasnt spawned, spawn user
+    user = User(username=name)
+    statement = select(User).where(User.username == name)  # uh
+    user = session.exec(statement).first()
+    user.cloudflare_turnstiles_solved += 1
+
+    if not user:  # then user not existy
+        user = User(username=name)
+        session.add(user)
+
+    user.cloudflare_turnstiles_solved += 1
+    print("did it add")
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    # yay we refreshed user
+    return user
+
+
+# @app.post("/captchasolved/")
+# def check_cf_turnstile(user: User, session: SessionDep):
+#     None
+
+
+# def create_hero(hero: Hero, session: SessionDep) -> Hero:
+#     session.add(hero)
+#     session.commit()
+#     session.refresh(hero)
+#     return hero
+
+
+# @app.post("/heroes/")
+# def create_hero(hero: Hero, session: SessionDep) -> Hero:
+#     session.add(hero)
+#     session.update
+#     session.commit()
+#     session.refresh(hero)
+#     return hero
+
+
+# @app.get("/heroes/")
+# def read_heroes(
+#     session: SessionDep,
+#     offset: int = 0,
+#     limit: Annotated[int, Query(le=100)] = 100,
+# ) -> list[Hero]:
+#     heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+#     return heroes
+
+
+# @app.get("/heroes/{hero_id}")
+# def read_hero(hero_id: int, session: SessionDep) -> Hero:
+#     hero = session.get(Hero, hero_id)
+#     if not hero:
+#         raise HTTPException(status_code=404, detail="Hero not found")
+#     return hero
+
+
+# @app.delete("/heroes/{hero_id}")
+# def delete_hero(hero_id: int, session: SessionDep):
+#     hero = session.get(Hero, hero_id)
+#     if not hero:
+#         raise HTTPException(status_code=404, detail="Hero not found")
+#     session.delete(hero)
+#     session.commit()
+#     return {"ok": True}
