300ba67a6a71aae8ab045830b4e3c26dfe75afec
m <test@example.com>
2026-05-31 17:05:52 +0700

    67 lines long main.py

--- main.py
+++ main.py
@@ -14,13 +14,28 @@ import aiohttp
 # aiohttp ssl sob macos issues ahhh
 import certifi
 import ssl
-from models import User
-from database import SessionDep, create_db_and_tables
-from config import CF_SECRET_KEY
 
 ssl_ctx = ssl.create_default_context(
     cafile=certifi.where()
 )  # apparently removing this still breaks things uh
+
+# custom omdules for now
+
+# misc config + core
+from models import User
+from database import SessionDep, create_db_and_tables
+from config import CF_SECRET_KEY
+
+# routes
+from api import leaderboard as api_leaderboard
+from api import captchas as api_captchas
+
+# pages
+from views import leaderboard as view_leaderboard
+from views import challenges as view_challenges
+
+# i uh am relying on vscode's "problems" tab without actually verifying so uh gl to me
+
 app = FastAPI()
 # code based on example from fastapi wesbsite
 
@@ -38,107 +53,14 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/api/leaderboard")
-async def get_leaderboard(session: SessionDep):
-    statement = select(User).order_by(desc(User.cloudflare_turnstiles_solved))
-    users = session.exec(statement).all()
-    print(users)
-    return users
+# api routers
+app.include_router(api_leaderboard.router)
 
 
-@app.get("/api/total_captchas")
-async def get_leaderboard(session: SessionDep):
-    total_statement = select(func.sum(User.cloudflare_turnstiles_solved))
-    total_solved = session.exec(total_statement).one()
-    return total_solved
+app.include_router(api_captchas.router)  # :3
 
+# page/view roters
 
-templates = Jinja2Templates(
-    directory="templates/"
-)  # todo -  move everything to template
+app.include_router(view_leaderboard.router)
 
-
-# @app.get("/captchas/challenge/cf-turnstile")
-@app.get("/leaderboard")
-async def serveTurnstile(request: Request, session: SessionDep):
-    # a
-    # todo: serve htmls
-    # return True
-    statement = (
-        select(User).order_by(desc(User.cloudflare_turnstiles_solved))
-        # .limit(100)  # limit 100 t?
-    )
-    # users = session.exec(statement).all()
-    users = session.exec(statement).all()
-    total_statement = select(func.sum(User.cloudflare_turnstiles_solved))
-    total_solved = session.exec(total_statement).one()
-    # print(users)s
-    return templates.TemplateResponse(
-        request=request,
-        name="leaderboard.html",
-        context={"users": users, "total_solved": total_solved},
-    )
-
-
-# challenge_pages = Jinja2Templates(directory="src/captchas/challenges")
-
-
-# @app.get("/captchas/challenge/cf-turnstile")
-@app.get("/captchas/cf-turnstile")
-async def serveTurnstile(name: str, request: Request):
-    # a
-    # todo: serve htmls
-    # return True
-    print(name)
-    return templates.TemplateResponse(
-        request=request, name="challenges/cf-turnstile.html", context={"name": name}
-    )
-
-
-@app.post("/captchas/verify/cf-turnstile")
-async def verifyCFTurnstile(name: str, data: dict, session: SessionDep):  # data: dict):
-    # this is NOT the right approach to get query and body lol
-    # ig i need types and stuff, tmr
-    # rn its just curl -X POST  "localhost:8000/captchas/verify/cf-turnstile?token=mrrp"
-    token = data.get("token")
-    print(token)
-
-    # fix body logic above
-
-    # cf: POST https://challenges.cloudflare.com/turnstile/v0/siteverify
-    url = "https://challenges.cloudflare.com/turnstile/v0/siteverify"
-    data = {
-        "secret": CF_SECRET_KEY,  # secret, i spent too long doing dotenv
-        "response": token,
-    }
-    async with aiohttp.ClientSession() as aiosession:
-        async with aiosession.post(url=url, data=data, ssl=ssl_ctx) as response:
-            try:
-                response.raise_for_status()
-                response_json = await response.json()
-                print(response_json)
-                if response_json["success"] == True:
-                    None
-                    # user cf count ++
-                    user = User(username=name)
-                    statement = select(User).where(User.username == name)  # uh
-                    user = session.exec(statement).first()
-
-                    if not user:  # then user not existy
-                        user = User(username=name)
-                        session.add(user)
-
-                    user.cloudflare_turnstiles_solved += 1
-                    print("did it add to ", user)
-                    session.add(user)
-                    session.commit()
-                    session.refresh(user)
-                response_json["uesrname"] = name
-                return response_json
-            except Exception as e:
-                print(f"Turnstile validation error: {e}")
-                return {
-                    "success": False,
-                    "error-codes": ["internal-error"],
-                    "username": name,
-                }
+app.include_router(view_challenges.router)
