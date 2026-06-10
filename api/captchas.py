20b7b413a7f1ea1a92d8689ceb9d8e06d49543eb
m <test@example.com>
2026-06-10 11:39:10 +0700

    feat: add recaptcha v2

--- api/captchas.py
+++ api/captchas.py
@@ -8,7 +8,7 @@ from sqlmodel import select, desc, func
 from models import User
 from database import SessionDep
 
-from config import CF_SECRET_KEY
+from config import CF_SECRET_KEY, RECAPTCHA_V2_SECRET_KEY
 import aiohttp
 
 # aiohttp ssl sob macos issues ahhh
@@ -82,3 +82,49 @@ async def verify_cf_turnstile(name: str, data: dict, session: SessionDep):
             "error-codes": ["internal-error"],
             "username": name,
         }
+
+
+# on hold until database is ready
+
+# @router.post("/verify/recaptcha-v2")
+# async def verify_recaptcha_v2(name: str, data: dict, session: SessionDep):
+#     name = clean_data(name)
+#     token = data.get("token")
+#     print(token)
+
+#     url = "https://www.google.com/recaptcha/api/siteverify"
+#     data = {
+#         "secret": RECAPTCHA_V2_SECRET_KEY,  # secret, i spent too long doing dotenv
+#         "response": token,
+#     }
+
+#     try:
+#         async with aiohttp.ClientSession() as aiosession:
+#             async with aiosession.post(url=url, data=data, ssl=ssl_ctx) as response:
+#                 response.raise_for_status()
+#                 response_json = await response.json()
+#                 print(response_json)
+#                 # on hold until database is ready
+#                 # if response_json["success"] == True:
+#                 #     user = User(username=name)
+#                 #     statement = select(User).where(User.username == name)  # uh
+#                 #     user = session.exec(statement).first()
+
+#                 #     if not user:  # then user not existy
+#                 #         user = User(username=name)
+#                 #         session.add(user)
+
+#                 #     user.cloudflare_turnstiles_solved += 1
+#                 #     print("did it add to ", user)
+#                 #     session.add(user)
+#                 #     session.commit()
+#                 #     session.refresh(user)
+#                 response_json["username"] = name
+#                 return response_json
+#     except Exception as e:
+#         print(f"Recaptcha validation error: {e}")
+#         return {
+#             "success": False,
+#             "error-codes": ["internal-error"],
+#             "username": name,
+#         }
